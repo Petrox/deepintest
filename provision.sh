1c2fb71cc0f6ee9b7e2bd494066ee05813961e92
Peter Kopias <kopias.peter@gmail.com>
2019-09-12 01:39:15 +0200

    non interactive install

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 add-apt-repository ppa:leaeasy/dde
 apt-get remove lightdm
-apt-get install -y dde dde-file-manager docker.io firefox mc netcat snapd git gdm3 deepin-gtk-theme
+DEBIAN_FRONTEND=noninteractive apt-get install -y dde dde-file-manager docker.io firefox mc netcat snapd git gdm3 deepin-gtk-theme
 apt-get autoremove -y
 adduser vagrant docker
 
