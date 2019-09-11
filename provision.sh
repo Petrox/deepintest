0e4886a07d7a2270add30e0d70464ab0fbe55d97
Peter Kopias <kopias.peter@gmail.com>
2019-09-12 00:15:13 +0200

    commandlinefix

--- provision.sh
+++ provision.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo add-apt-repository ppa:leaeasy/dde
-sudo apt-get update
+add-apt-repository ppa:leaeasy/dde
 apt-get install -y dde docker.io mc netcat snapd git 
-sudo adduser docker vagrant
+adduser vagrant docker
 
 
