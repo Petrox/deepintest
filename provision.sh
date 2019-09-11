4154f09e3cb5e0c01303e366d0ed9e5821349237
Peter Kopias <kopias.peter@gmail.com>
2019-09-12 00:39:34 +0200

    tweak packages

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 add-apt-repository ppa:leaeasy/dde
-apt-get install -y dde docker.io mc netcat snapd git 
+apt-get remove lightdm
+apt-get install -y dde dde-file-manager docker.io mc netcat snapd git gdm3 deepin-gtk-theme
+apt-get autoremove -y
 adduser vagrant docker
+
 
 
