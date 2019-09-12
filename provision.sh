d90e7b93e223fec41716b73ab242387a06061d11
Peter Kopias <kopias.peter@gmail.com>
2019-09-12 02:53:14 +0200

    simplified

--- provision.sh
+++ provision.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sed -i -e 's/http:\/\/us.archive/mirror:\/\/mirrors/' -e 's/\/ubuntu\//\/mirrors.txt/' /etc/apt/sources.list
-add-apt-repository ppa:leaeasy/dde
-apt-get remove lightdm
-apt-get dist-upgrade -y
-apt-get autoremove -y
 debconf-set-selections << ENDIT
 kdm	shared/default-x-display-manager	select	gdm
 lightdm	shared/default-x-display-manager	select	gdm
 gdm3	shared/default-x-display-manager	select	gdm
 ENDIT
-DEBIAN_FRONTEND=noninteractive apt-get install -y dde dde-file-manager firefox mc netcat git gdm3 deepin-gtk-theme
+sed -i -e 's/http:\/\/us.archive/mirror:\/\/mirrors/' -e 's/\/ubuntu\//\/mirrors.txt/' /etc/apt/sources.list
+sed -i -e 's/http:\/\/us.archive/mirror:\/\/mirrors/' -e 's/\/ubuntu\//\/mirrors.txt/' /etc/apt/sources.list.d/*
+add-apt-repository ppa:leaeasy/dde
+apt-get remove lightdm
+# apt-get dist-upgrade -y ; apt-get autoremove -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y dde dde-file-manager firefox mc netcat git gdm3 deepin-gtk-theme gcc make perl
 apt-get autoremove -y
 
 
