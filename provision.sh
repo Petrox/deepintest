75598474cf4cfcf774d21db75b0c166835567ef0
Peter Kopias <kopias.peter@gmail.com>
2019-09-12 02:15:00 +0200

    forced install config distupgrade and autoremove

--- provision.sh
+++ provision.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 add-apt-repository ppa:leaeasy/dde
 apt-get remove lightdm
+apt-get dist-upgrade -y
+apt-get autoremove -y
+debconf-set-selections << ENDIT
+kdm	shared/default-x-display-manager	select	gdm
+lightdm	shared/default-x-display-manager	select	gdm
+gdm3	shared/default-x-display-manager	select	gdm
+ENDIT
 DEBIAN_FRONTEND=noninteractive apt-get install -y dde dde-file-manager docker.io firefox mc netcat snapd git gdm3 deepin-gtk-theme
 apt-get autoremove -y
 adduser vagrant docker
